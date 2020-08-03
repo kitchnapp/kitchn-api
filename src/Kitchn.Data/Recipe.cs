c6f89e49bc52c6b3fb97cc92fdc875383c7a3b4b
Keir Davis <keirjdavis@gmail.com>
2020-08-03 12:44:26 +0100

    feat(data): add description and rating to recipe model

--- src/Kitchn.Data/Recipe.cs
+++ src/Kitchn.Data/Recipe.cs
@@ -18,8 +18,18 @@ namespace Kitchn.Data
 		public string Name { get; set; }
 
 		/// <summary>
+		/// Description of this recipe
+		/// </summary>
+		public string Description { get; set; }
+
+		/// <summary>
 		/// Instruction Id's of this recipe
 		/// </summary>
 		public List<Guid> RecipeInstructionIds { get; set; }
+
+		/// <summary>
+		/// Rating of this recipe
+		/// </summary>
+		public int Rating { get; set; }
 	}
 }
