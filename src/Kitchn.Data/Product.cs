aab7392a3c8b0eb1a0b8f4221903b56f587fff97
Keir Davis <keirjdavis@gmail.com>
2020-08-03 12:27:34 +0100

    feat(data): add data project for models

---
+++ src/Kitchn.Data/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Kitchn.Data
+{
+	/// <summary>
+	/// Product model
+	/// </summary>
+	public class Product
+	{
+		/// <summary>
+		/// ID of this product
+		/// </summary>
+		public Guid Id { get; set; }
+
+		/// <summary>
+		/// Name of this product
+		/// </summary>
+		public string Name { get; set; }
+	}
+}
