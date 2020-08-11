c5f4c149bccce3ea931191f25910a29b65fa63b4
Keir Davis <keirjdavis@gmail.com>
2020-08-11 11:40:14 +0100

    feat(data): add measurement conversions model

---
+++ src/Kitchn.Data/Models/MeasurementConversions.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace Kitchn.Data.Models
+{
+	/// <summary>
+	/// Measurement Conversions model
+	/// </summary>
+	public class MeasurementConversions
+	{
+		/// <summary>
+		/// ID of this measurement conversion
+		/// </summary>
+		public Guid Id { get; set; }
+
+		/// <summary>
+		/// From Measurement ID
+		/// </summary>
+		public Guid FromMeasurementId { get; set; }
+
+		/// <summary>
+		/// To Measurement ID
+		/// </summary>
+		public Guid ToMeasurementId { get; set; }
+
+		/// <summary>
+		/// Factor between the measurements to go from one measurement to the other
+		/// </summary>
+		public decimal Factor { get; set; }
+
+		/// <summary>
+		/// Calculate the to-measurement value
+		/// </summary>
+		public decimal Calculate(decimal fromMeasurementValue)
+		{
+			return fromMeasurementValue * Factor;
+		}
+
+		/// <summary>
+		/// Calculate the reverse from-measurement value
+		/// </summary>
+		public decimal ReverseCalculate(decimal toMeasurementValue)
+		{
+			return toMeasurementValue / Factor;
+		}
+	}
+}
