997af694a97324c4b3ab0fb6574c9e1c03234e39
Keir Davis <keirjdavis@gmail.com>
2020-08-29 16:10:23 +0100

    feat(location): add locations to query

--- src/Kitchn.API.GraphQL/Models/KitchnQuery.cs
+++ src/Kitchn.API.GraphQL/Models/KitchnQuery.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using GraphQL;
 using GraphQL.Types;
 
@@ -22,6 +23,43 @@ namespace Kitchn.API.GraphQL.Models
 					{
 						Id = id,
 						Name = "Test"
+					};
+				}
+			);
+
+			Field<LocationType>(
+				"location",
+				"Get location by ID",
+				arguments: new QueryArguments(
+					new QueryArgument<NonNullGraphType<IdGraphType>> { Name = "id" }
+				),
+				resolve: context =>
+				{
+					var id = context.GetArgument<Guid>("id");
+
+					return new Product
+					{
+						Id = id,
+						Name = "Test Location"
+					};
+				}
+			);
+
+			Field<ListGraphType<LocationType>>(
+				"locations",
+				"Get a list of locations",
+				arguments: new QueryArguments(
+					new QueryArgument<StringGraphType> { Name = "search" }
+				),
+				resolve: context =>
+				{
+					var search = context.GetArgument<string>("search");
+
+					return new List<Location>{
+						new Location {
+							Id = Guid.NewGuid(),
+							Name = "Example"
+						}
 					};
 				}
 			);
