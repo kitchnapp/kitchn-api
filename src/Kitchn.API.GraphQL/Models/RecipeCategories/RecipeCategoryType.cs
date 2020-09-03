266f4b70c6e955ef81476eec3c4beb99dce9d789
Keir Davis <keirjdavis@gmail.com>
2020-09-03 11:29:51 +0100

    feat(recipecategories): add recipes child to categories

--- src/Kitchn.API.GraphQL/Models/RecipeCategories/RecipeCategoryType.cs
+++ src/Kitchn.API.GraphQL/Models/RecipeCategories/RecipeCategoryType.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using GraphQL;
 using GraphQL.Types;
 using Kitchn.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Kitchn.API.GraphQL.Models.RecipeCategories
 {
@@ -14,6 +15,28 @@ namespace Kitchn.API.GraphQL.Models.RecipeCategories
 
 			Field(x => x.Id).Description("The ID of the recipe category.");
 			Field(x => x.Name).Description("The name of the recipe category.");
+
+			Field<ListGraphType<Models.Recipes.RecipeType>>("recipes", "The recipes in this category.",
+				arguments: new QueryArguments(
+					new QueryArgument<StringGraphType> { Name = "search" }
+				),
+				resolve: context =>
+				{
+					var search = context.GetArgument<string>("search");
+
+					return dbContext.Recipes
+							.Include(q => q.Categories)
+							.Where(q => q.Name.Contains(search) || search == null)
+							.Where(q => q.Categories.Where(q => q.RecipeCategoryId == context.Source.Id).Any())
+							.Select(recipe => new Recipes.Recipe
+							{
+								Id = recipe.Id,
+								Name = recipe.Name,
+								Description = recipe.Description,
+								Rating = recipe.Rating
+							});
+				}
+			);
 		}
 	}
 }
