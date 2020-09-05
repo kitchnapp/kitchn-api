cecefba389ecd7d4fabd6e527a9074cec5d8db87
Keir Davis <keirjdavis@gmail.com>
2020-09-05 13:26:11 +0100

    feat(recipetype): add recipe category sub graph

--- src/Kitchn.API.GraphQL/Models/Recipes/RecipeType.cs
+++ src/Kitchn.API.GraphQL/Models/Recipes/RecipeType.cs
@@ -1,11 +1,14 @@
+using System.Linq;
 using GraphQL;
 using GraphQL.Types;
+using Kitchn.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Kitchn.API.GraphQL.Models.Recipes
 {
 	public class RecipeType : ObjectGraphType<Recipe>
 	{
-		public RecipeType()
+		public RecipeType(KitchnDbContext dbContext)
 		{
 			Name = "Recipe";
 			Description = "A recipe to follow to make a meal.";
@@ -14,6 +17,21 @@ namespace Kitchn.API.GraphQL.Models.Recipes
 			Field(x => x.Name).Description("The name of the recipe.");
 			Field(x => x.Description).Description("The description of the recipe.");
 			Field<IntGraphType>("rating", "The rating of the recipe.");
+
+			Field<ListGraphType<RecipeCategories.RecipeCategoryType>>("categories", "The categories of the recipe.",
+				resolve: context =>
+				{
+					return dbContext.Recipes
+						.Include(o => o.Categories)
+						.Where(q => q.Id == context.Source.Id)
+						.SelectMany(recipe => recipe.Categories)
+						.Select(categoryLink => new RecipeCategories.RecipeCategory
+						{
+							Id = categoryLink.RecipeCategory.Id,
+							Name = categoryLink.RecipeCategory.Name
+						});
+				}
+			);
 		}
 	}
 }
