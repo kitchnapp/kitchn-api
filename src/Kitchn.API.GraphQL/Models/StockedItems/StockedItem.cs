297ce3d92a3ab06bdaa343432f26d30b4a12ed15
Keir Davis <keirjdavis@gmail.com>
2020-09-12 23:08:02 +0100

    feat(stockeditem): add stockeditem model

---
+++ src/Kitchn.API.GraphQL/Models/StockedItems/StockedItem.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Kitchn.API.GraphQL.Models.StockedItems
+{
+	public class StockedItem
+	{
+		public Guid Id { get; set; }
+		public Guid ProductId { get; set; }
+		public Guid? LocationId { get; set; }
+		public DateTime? ExpiryDate { get; set; }
+		public DateTime? OpenedDate { get; set; }
+	}
+}
