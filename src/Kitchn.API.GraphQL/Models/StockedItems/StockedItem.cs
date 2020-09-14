f956aaae15286b3fd6f0b5ea6bef9b76c0d39a86
Keir Davis <keirjdavis@gmail.com>
2020-09-14 09:51:19 +0100

    fix(stockeditem): fix productid to be nullable in model

--- src/Kitchn.API.GraphQL/Models/StockedItems/StockedItem.cs
+++ src/Kitchn.API.GraphQL/Models/StockedItems/StockedItem.cs
@@ -5,7 +5,7 @@ namespace Kitchn.API.GraphQL.Models.StockedItems
 	public class StockedItem
 	{
 		public Guid Id { get; set; }
-		public Guid ProductId { get; set; }
+		public Guid? ProductId { get; set; }
 		public Guid? LocationId { get; set; }
 		public DateTime? ExpiryDate { get; set; }
 		public DateTime? OpenedDate { get; set; }
