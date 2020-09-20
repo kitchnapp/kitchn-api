fc4bb5de17c309d191af0a86755b29d79e2d57d3
Keir Davis <keirjdavis@gmail.com>
2020-09-20 09:32:01 +0100

    feat(recipeinstructiontype): add instruction gql type

---
+++ src/Kitchn.API.GraphQL/Models/RecipeInstructions/RecipeInstructionType.cs
@@ -0,0 +1,37 @@
+using System.Linq;
+using GraphQL;
+using GraphQL.Types;
+using Kitchn.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kitchn.API.GraphQL.Models.RecipeInstructions
+{
+	public class RecipeInstructionType : ObjectGraphType<RecipeInstruction>
+	{
+		public RecipeInstructionType(KitchnDbContext dbContext)
+		{
+			Name = "RecipeInstruction";
+			Description = "A instruction for a recipe.";
+
+			Field(x => x.Id).Description("The ID of the recipe instruction.");
+			Field(x => x.Order).Description("The order of the recipe instruction.");
+			Field(x => x.Instructions).Description("The instruction of the step.");
+
+			Field<Models.Recipes.RecipeType>("recipe", "The recipe for this instruction.",
+				resolve: context =>
+				{
+					return dbContext.Recipes
+						.Where(q => q.Id == context.Source.RecipeId)
+						.Select(recipe => new Recipes.Recipe
+						{
+							Id = recipe.Id,
+							Name = recipe.Name,
+							Description = recipe.Description,
+							Rating = recipe.Rating
+						})
+						.FirstOrDefault();
+				}
+			);
+		}
+	}
+}
