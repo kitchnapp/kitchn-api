348afbab564f7eead1f15d4801e5772531716a33
Keir Davis <keirjdavis@gmail.com>
2020-09-20 09:27:26 +0100

    feat(recipeinstruction): add recipe instruction object

---
+++ src/Kitchn.API.GraphQL/Models/RecipeInstructions/RecipeInstruction.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Kitchn.API.GraphQL.Models.RecipeInstructions
+{
+	public class RecipeInstruction
+	{
+		public Guid Id { get; set; }
+		public Guid? RecipeId { get; set; }
+		public int? Order { get; set; }
+		public string Instructions { get; set; }
+	}
+}
