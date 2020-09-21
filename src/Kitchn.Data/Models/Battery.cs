91d128878e593cb48d7450f62100c6d3564159b6
Keir Davis <keirjdavis@gmail.com>
2020-09-21 12:04:51 +0100

    feat(battery): add battery db model

---
+++ src/Kitchn.Data/Models/Battery.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace Kitchn.Data.Models
+{
+	public class Battery
+	{
+		/// <summary>
+		/// ID of this battery
+		/// </summary>
+		public Guid Id { get; set; }
+
+		/// <summary>
+		/// Name of the battery
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Where this battery currently is situated
+		/// </summary>
+		public string Location { get; set; }
+
+		/// <summary>
+		/// Battery type
+		/// </summary>
+		public string Type { get; set; }
+
+		/// <summary>
+		/// Last charged
+		/// </summary>
+		public DateTime? LastCharged { get; set; }
+
+		/// <summary>
+		/// Whether it is a rechargeable battery
+		/// </summary>
+		public bool? Rechargeable { get; set; }
+
+		/// <summary>
+		/// Some batteries have an expiry date on them
+		/// </summary>
+		public DateTime? ExpiryDate { get; set; }
+	}
+}
