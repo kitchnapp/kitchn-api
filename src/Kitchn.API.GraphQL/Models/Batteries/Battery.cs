dd456e3460606357df9ff117813e2d578e1258b8
Keir Davis <keirjdavis@gmail.com>
2020-09-21 12:38:11 +0100

    feat(battery): add api model

---
+++ src/Kitchn.API.GraphQL/Models/Batteries/Battery.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Kitchn.API.GraphQL.Models.Batteries
+{
+	public class Battery
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+		public string Location { get; set; }
+		public DateTime? LastCharged { get; set; }
+		public bool? Rechargeable { get; set; }
+		public DateTime? ExpiryDate { get; set; }
+	}
+}
