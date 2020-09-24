ee80118593d461291897864b289fd5117e187d13
Keir Davis <keirjdavis@gmail.com>
2020-09-24 14:55:38 +0100

    feat(migrations): add consumed count migration

---
+++ src/Kitchn.Data/Migrations/20200924135519_ConsumedCount.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kitchn.Data.Migrations
+{
+    public partial class ConsumedCount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ConsumedCount",
+                table: "StockedItems",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConsumedCount",
+                table: "StockedItems");
+        }
+    }
+}
