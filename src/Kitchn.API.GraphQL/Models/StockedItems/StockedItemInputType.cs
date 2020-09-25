cde19b895b29bcf6bb7c02c8c8e6d66f66a31294
Keir Davis <keirjdavis@gmail.com>
2020-09-25 17:40:45 +0100

    fix(stockeditemtype): fix expiry and opened types

--- src/Kitchn.API.GraphQL/Models/StockedItems/StockedItemInputType.cs
+++ src/Kitchn.API.GraphQL/Models/StockedItems/StockedItemInputType.cs
@@ -12,8 +12,8 @@ namespace Kitchn.API.GraphQL.Models.StockedItems
 			Field(x => x.ProductId, nullable: true).Description("The product of the stocked item.");
 			Field(x => x.LocationId, nullable: true).Description("The location of the stocked item.");
 			Field<IntGraphType>("consumedCount", "The amount of times this item has been consumed.");
-			Field<TimeSpanSecondsGraphType>("expiryDate", "The expiry date for the stocked item.");
-			Field<TimeSpanSecondsGraphType>("openedDate", "The date the stocked item was opened.");
+			Field<DateTimeGraphType>("expiryDate", "The expiry date for the stocked item.");
+			Field<DateTimeGraphType>("openedDate", "The date the stocked item was opened.");
 		}
 	}
 }
--- src/Kitchn.API.GraphQL/Models/StockedItems/StockedItemType.cs
+++ src/Kitchn.API.GraphQL/Models/StockedItems/StockedItemType.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using AutoMapper;
 using GraphQL;
 using GraphQL.Types;
 using Kitchn.Data;
@@ -8,44 +9,35 @@ namespace Kitchn.API.GraphQL.Models.StockedItems
 {
 	public class StockedItemType : ObjectGraphType<StockedItem>
 	{
-		public StockedItemType(KitchnDbContext dbContext)
+		public StockedItemType(KitchnDbContext dbContext, IMapper mapper)
 		{
 			Name = "StockedItem";
 			Description = "An item in stock.";
 
 			Field(x => x.Id).Description("The ID of the stocked item.");
 			Field<IntGraphType>("consumedCount", "The amount of times this item has been consumed.");
-			Field<TimeSpanSecondsGraphType>("expiryDate", "The expiry date for the stocked item.");
-			Field<TimeSpanSecondsGraphType>("openedDate", "The date the stocked item was opened.");
+			Field<DateTimeGraphType>("expiryDate", "The expiry date for the stocked item.");
+			Field<DateTimeGraphType>("openedDate", "The date the stocked item was opened.");
 
 			Field<Products.ProductType>("product", "The stocked product.",
 				resolve: context =>
 				{
-					return dbContext.Products
-						.Where(q => q.Id == context.Source.ProductId)
-						.Select(product => new Products.Product
-						{
-							Id = product.Id,
-							Name = product.Name,
-							DefaultBestBefore = product.DefaultBestBefore,
-							DefaultConsumeWithin = product.DefaultConsumeWithin,
-							DefaultLocationId = product.DefaultLocationId
-						})
-						.FirstOrDefault();
+					return mapper.Map<Products.Product>(
+						dbContext.Products
+							.Where(q => q.Id == context.Source.ProductId)
+							.FirstOrDefault()
+					);
 				}
 			);
 
 			Field<Locations.LocationType>("location", "Where the stocked product is located.",
 				resolve: context =>
 				{
-					return dbContext.Locations
-						.Where(q => q.Id == context.Source.LocationId)
-						.Select(location => new Locations.Location
-						{
-							Id = location.Id,
-							Name = location.Name,
-						})
-						.FirstOrDefault();
+					return mapper.Map<Locations.Location>(
+						dbContext.Locations
+							.Where(q => q.Id == context.Source.LocationId)
+							.FirstOrDefault()
+					);
 				}
 			);
 		}
