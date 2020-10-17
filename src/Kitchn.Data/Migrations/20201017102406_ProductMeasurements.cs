dc10ca80664f8821e2c92ca23ff0f94077c69545
Keir Davis <keirjdavis@gmail.com>
2020-10-17 11:53:18 +0100

    feat(migrations): add product measurement migration

---
+++ src/Kitchn.Data/Migrations/20201017102406_ProductMeasurements.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Kitchn.Data.Migrations
+{
+    public partial class ProductMeasurements : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductMeasurements");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "GroupMeasurementId",
+                table: "Products",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "IndividualMeasurementId",
+                table: "Products",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "Measurements",
+                columns: new[] { "Id", "MultipleName", "Name" },
+                values: new object[] { new Guid("3afacf1b-d116-4622-a188-d0c36918efa9"), null, "Tablet" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_GroupMeasurementId",
+                table: "Products",
+                column: "GroupMeasurementId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_IndividualMeasurementId",
+                table: "Products",
+                column: "IndividualMeasurementId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Measurements_GroupMeasurementId",
+                table: "Products",
+                column: "GroupMeasurementId",
+                principalTable: "Measurements",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Measurements_IndividualMeasurementId",
+                table: "Products",
+                column: "IndividualMeasurementId",
+                principalTable: "Measurements",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Measurements_GroupMeasurementId",
+                table: "Products");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Measurements_IndividualMeasurementId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_GroupMeasurementId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_IndividualMeasurementId",
+                table: "Products");
+
+            migrationBuilder.DeleteData(
+                table: "Measurements",
+                keyColumn: "Id",
+                keyValue: new Guid("3afacf1b-d116-4622-a188-d0c36918efa9"));
+
+            migrationBuilder.DropColumn(
+                name: "GroupMeasurementId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "IndividualMeasurementId",
+                table: "Products");
+
+            migrationBuilder.CreateTable(
+                name: "ProductMeasurements",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Factor = table.Column<int>(type: "integer", nullable: true),
+                    GroupMeasurementId = table.Column<Guid>(type: "uuid", nullable: true),
+                    IndividualMeasurementId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductMeasurements", x => x.Id);
+                });
+        }
+    }
+}
